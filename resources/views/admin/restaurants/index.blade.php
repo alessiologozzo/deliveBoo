12ce4c1c722bd8cf3f9a73560c384c5be3350800
taddeoneri <taddeo.neri10@gmail.com>
2023-06-30 11:19:50 +0200

    restaurant crud

--- resources/views/admin/restaurants/index.blade.php
+++ resources/views/admin/restaurants/index.blade.php
@@ -1,11 +1,8 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="text-center">
-        <h1 class="text-center">Restaurant List</h1>
-
-        {{-- <a class="btn btn-success text-white" href="{{ route('admin.restaurants.create') }}">New Restaurant</a> --}}
-
+    <div class="text-center  my-4">
+        <h1 class="text-center">Restaurant</h1>
     </div>
 
     {{-- @if (session()->has('message'))
@@ -13,43 +10,29 @@
             {{ session()->get('message') }}
         </div>
     @endif --}}
-    <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Name</th>
-                <th scope="col">Logo</th>
-                <th scope="col">Address</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($restaurants as $restaurant)
-                <tr>
-                    <th scope="row">{{ $restaurant->id }}</th>
-                    <td>{{ $restaurant->name }}</td>
-                    <td><img class="img-thumbnail bg-secondary" style="width:100px" class="bg-black" src="{{ asset('storage/' . $restaurant->logo) }}"
-                            alt="{{ $restaurant->name }}">
-                    </td>
-                    <td>{{ $restaurant->address }}</td>
-                    {{-- <td>
-                        {{ $restaurant->category ? $restaurant->category->name : 'Senza categoria' }}
-                    </td> --}}
-                    <td>
-                        <div class="d-flex justify-content-between align-items-center">
-                            <a href="{{ route('admin.restaurants.show', $restaurant->slug) }}"
-                                class="btn btn-primary text-white">SHOW</a>
-                            <a href="{{ route('admin.restaurants.edit', $restaurant->slug) }}"
-                                class="btn btn-warning text-white">EDIT</a>
-                            <form action="{{ route('admin.restaurants.destroy', $restaurant->slug) }}" method="POST">
-                                @csrf
-                                @method('DELETE')
-                                <button type='submit' class="delete-button btn btn-danger text-white"
-                                    data-item-title="{{ $restaurant->name }}">DELETE</button>
-                            </form>
-                        </div>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+    @foreach ($restaurants as $restaurant)
+    <div class=" d-flex justify-content-center">
+        <div class="card w-25 d-flex flex-column align-items-center" style="box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px">
+            <img src="{{ asset('storage/' . $restaurant->logo) }}" class="card-img-top w-50 my-3" alt="{{$restaurant->name}}">
+            <div class="card-body">
+                <h5 class="card-title text-center">{{$restaurant->name}}</h5>
+                <p class="card-text text-center">{{$restaurant->address}}</p>
+                <div class="d-flex justify-content-between">
+                    <a href="{{ route('admin.restaurants.edit', $restaurant->slug) }}"
+                        class="btn btn-warning text-white">EDIT</a>
+                    <form action="{{ route('admin.restaurants.destroy', $restaurant->slug) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type='submit' class="delete-button btn btn-danger text-white"
+                            data-item-title="{{ $restaurant->name }}">DELETE</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    @endforeach
+
 @endsection
+
+
