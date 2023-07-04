e2337e1e7bd8d5600f19c27a9b51d57bcf3a83c9
DavideSberna <sbernadavide@gmail.com>
2023-07-05 01:33:44 +0200

    complete branch

--- resources/views/admin/dishes/show.blade.php
+++ resources/views/admin/dishes/show.blade.php
@@ -12,32 +12,32 @@
             <!-- col-8 -->
             <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-7 p-3">
                <div>
-                  <img class="w-100" src="{{ asset('storage/' . $dish->image) }}" alt="{{ $dish->name }}">
+                  <img class="w-100 rounded rounded-3" src="{{ asset('storage/' . $dish->image) }}" alt="{{ $dish->name }}">
                </div>
             </div>
             <div class="col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-4 p-3">
                <div class="row">
                   <div class="col-6 pb-4">
-                     <div class="card p-3">
-                        <p>Total sell:</p>
-                        <p class="m-0 fs-1">{{ $totalAmount }} <span class="fs-5">€</span></p>
+                     <div class="card p-3 first-dashboard-card border border-0">
+                        <p class="fs-4 text-white">Total sell:</p>
+                        <p class="m-0 fs-1 text-white">{{ $totalAmount }} <span class="fs-5">€</span></p>
                      </div>
                   </div>
                   <div class="col-6 pb-4">
-                     <div class="card p-3">
-                        <p>Total order:</p>
-                        <p class="m-0 fs-1">{{ $orderCount }}</p>
+                     <div class="card p-3 second-dashboard-card border border-0">
+                        <p class="fs-4 text-white">Total order:</p>
+                        <p class="m-0 fs-1 text-white">{{ $orderCount }}</p>
                      </div>
                   </div>
                   <div class="col-6 pb-4">
-                     <div class="card p-3">
-                        <p>Total dish sell:</p>
-                        <p class="m-0 fs-1">{{ $totalDishes }}</p>
+                     <div class="card p-3 third-dashboard-card border border-0">
+                        <p class="fs-4 text-white">Total dish sell:</p>
+                        <p class="m-0 fs-1 text-white">{{ $totalDishes }}</p>
                      </div>
                   </div>
                   <div class="col-6 pb-4">
-                     <div class="card p-3 h-100">
-                        <p>Category:</p>
+                     <div class="card p-3 h-100 fourth-dashboard-card border border-0">
+                        <p class="fs-4 text-white">Category:</p>
                         <div>
                            <span class="badge text-bg-light">{{ $dish->category }}</span>
                         </div>
@@ -45,18 +45,18 @@
                   </div>
                </div>         
                <div class="">
-                  <div class="card p-3">
+                  <div class="card p-3 first-dashboard-card border border-0">
                      <div>
-                        <p class="fs-4">Description Dish</p>
+                        <p class="fs-4 text-white">Description Dish</p>
                      </div>      
-                     <p class="m-0 mb-2">Id: <span>{{ $dish->id }}</span></p>
-                     <p class="m-0 mb-2">Price: <span>{{ $dish->price }} euro</span></p>
+                     <p class="m-0 mb-2 text-white">Id: <span>{{ $dish->id }}</span></p>
+                     <p class="m-0 mb-2 text-white">Price: <span>{{ $dish->price }} euro</span></p>
                      @if($dish->visible > 0)
-                        <p class="m-0 mb-2">Visible: <span>yes</span></p>
+                        <p class="m-0 mb-2 text-white">Visible: <span>yes</span></p>
                      @else
-                        <p class="m-0 mb-2">Visible: <span>no</span></p>
+                        <p class="m-0 mb-2 text-white">Visible: <span>no</span></p>
                      @endif
-                     <p class="m-0 mb-2 pe-5">Description: <span>{{ $dish->description }}</span></p>
+                     <p class="m-0 mb-2 pe-5 text-white">Description: <span>{{ $dish->description }}</span></p>
                   </div>
                </div>
             </div>
@@ -72,7 +72,7 @@
             @foreach ($disheCategory as $item)   
             <div class="col-12 col-sm-6 col-md-4 col-lg-4 col-xxl-3">
                <div class="card">
-                  <img class="img-show-category img-fluid" src="{{ asset('storage/' . $item->image) }}" alt="{{ $item->name }}">
+                  <img class="img-show-category img-fluid rounded-top" src="{{ asset('storage/' . $item->image) }}" alt="{{ $item->name }}">
                   <div class="card-body">
                      {{ Str::limit($item->name, 15, '...') }}
                      <span class="badge rounded-pill bg-light text-dark mt-2 pt-2 pb-2">{{ $item->category }}</span>
@@ -89,19 +89,19 @@
          <div class="card">
             <div class="height-overflow overflow-auto">
                <table class="table">
-                  <thead>
-                     <tr>
-                        <th class="text-white">Image</th>
+                  <thead class="">
+                     <tr class="">
+                        <th class="text-white rounded-start">Image</th>
                         <th class="text-white">Name</th>
-                        <th class="text-white">Category</th>
+                        <th class="text-white rounded-end">Category</th>
                      </tr>
                   </thead>
                   <tbody>
                      @foreach ($dishes as $item)
-                     <tr>
-                        <td><a href="{{ route('dishes.show', $item->slug) }}"><img class="img-table p-1" src="{{ asset('storage/' . $item->image) }}" alt="{{ $item->name }}"></a></td>
+                     <tr class="">
+                        <td class=""><a href="{{ route('dishes.show', $item->slug) }}"><img class="img-table p-1" src="{{ asset('storage/' . $item->image) }}" alt="{{ $item->name }}"></a></td>
                         <th><a class="link-offset-2 link-underline link-underline-opacity-0 d-block pt-2 text-dark" href="{{ route('dishes.show', $item->slug) }}">{{ $item->name }}</a></th>
-                        <td><span class="badge rounded-pill bg-light text-dark mt-2 pt-2 pb-2">{{ $item->category }}</span></td>
+                        <td class=""><span class="badge rounded-pill bg-light text-dark mt-2 pt-2 pb-2">{{ $item->category }}</span></td>
                      </tr>
                      @endforeach
                   </tbody>
