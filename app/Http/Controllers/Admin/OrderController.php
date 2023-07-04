637ac530ffe4210131a1d5c3649cf9f98540492e
Gianmarco Incocciati <minusgian@gmail.com>
2023-07-04 18:41:10 +0200

    modify document

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -8,7 +8,6 @@ use App\Models\Order;
 use App\Models\Restaurant;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 
 class OrderController extends Controller
 {
